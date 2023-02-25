feb8cfb0313377636105f4f6dfd41cab30b98857
Luzefiru <ian.deejay.iv@gmail.com>
2023-02-25 22:22:51 +0800

    feat: add offcanvas sidebar functionality

--- client/src/script.js
+++ client/src/script.js
@@ -1,1 +1,13 @@
-// for sam
+/* Offcanvas Sidebar's Functionalities */
+const btnOffcanvasToggler = document.querySelector('.navbar-toggler');
+const offcanvasSidebar = document.querySelector('#offcanvas');
+const btnSidebarClose = document.querySelector(`.btn-close[data-bs-dismiss="offcanvas"]`);
+
+// allows the Burger Menu <button> display/collapse the sidebar
+btnOffcanvasToggler.addEventListener('click', () => {
+    offcanvasSidebar.classList.toggle('show');
+})
+// allows the Sidebar's Close Icon Button to collapse the sidebar
+btnSidebarClose.addEventListener('click', () => {
+    offcanvasSidebar.classList.toggle('show');
+})
